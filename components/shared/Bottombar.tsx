380c0594ce5a595ff96cb9286237633b1b7f37f4
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2024-02-23 18:20:09 -0800

    Added bottombar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 function Bottombar() {
-  return <h1>Bottombar</h1>;
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link transition-all hover:bg-purple-500 hover:bg-opacity-15 ${isActive && "bg-purple-500 hover:bg-opacity-80"}`}
+            >
+              <Image src={link.imgURL} width={25} height={25} alt="link logo" />
+              <p className="text-subtle-medium text-white max-sm:hidden">
+                {link.label.split(/\s+./)[0]}
+                </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Bottombar;
