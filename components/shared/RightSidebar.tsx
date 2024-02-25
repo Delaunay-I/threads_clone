729d26624c6c3a1ea902e77cd1b541dfbba07a96
Amirhossein <96101841+Delaunay-I@users.noreply.github.com>
2024-02-25 22:16:14 +0000

    Added right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,6 +1,14 @@
 function RightSidebar() {
-    return <h1>RightSidebar</h1>;
-  }
-  
-  export default RightSidebar;
-  
+  return (
+    <section className="sticky right-0 top-0 z-20 flex h-screen flex-col justify-between bg-dark-2 px-4 pb-5 pt-28 max-md:hidden">
+      <div className="">
+        <h3>Suggested Communities</h3>
+      </div>
+      <div>
+        <h3>Suggested Users</h3>
+      </div>
+    </section>
+  );
+}
+
+export default RightSidebar;
